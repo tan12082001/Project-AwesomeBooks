2c14b95c571245e723934caba566526ee28c87b4
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-07-05 15:28:18 +0430

    Add style to the title Add style to the book name and author Add style to form

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bookstorage = JSON.parse(localStorage.getItem('books-display')) || [];
 const bookname = document.getElementById('book-name');
 const authorname = document.getElementById('author-name');
 const addbutton = document.querySelector('.add-book-button');
-const presentbooks = document.querySelector('.books-display');
+const presentbooks = document.querySelector('#books');
 
 // Books Array class to store books
 class Books {
@@ -34,11 +34,10 @@ class Activity {
     for (let i = 0; i < storage.bookstorage.length; i += 1) {
       storage.bookstorage[i].id = (storage.bookstorage.indexOf(storage.bookstorage[i])) + 1;
       presentbooks.innerHTML += `
-      <ul>
-      <li>${storage.bookstorage[i].title}</li>
-      <li>${storage.bookstorage[i].author}</li>
-      <button class='re' type='button' id='${storage.bookstorage[i].id}'>remove</button>
-      </ul>
+        <tr>
+          <td class="btn-book-name">"${storage.bookstorage[i].title}" by ${storage.bookstorage[i].author} </td>
+          <td class="btn-remove"> <button class='re' type='button' id='${storage.bookstorage[i].id}'>remove</button> </td>
+        </tr>
       `;
     }
     // Event to call the remove method to remove book
